5badd8a42ea704e048231fccd346cf78090c9643
brladder77_mac <braldder77@naver.com>
2024-05-03 03:33:21 +0900

    push

--- standard_gpt.py
+++ standard_gpt.py
@@ -11,4 +11,4 @@ api_key = os.getenv("OPENAI_API_KEY")
 
 chat_model = ChatOpenAI(openai_api_key=api_key)
 
-chat_model.predict("잠이 안 올 때는 어떻게 하면 좋을지 대답해줘")
+chat_model.predict("잠이 안 올 때는 어떻게 하면 좋을지 대답해줘.")
